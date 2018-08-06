0dd03cc32be5a49ba5f20960c060fdb3066c341b
emergent <yosikawa@gmail.com>
2018-08-06 23:25:14 +0900

    solved problem007 using Python

---
+++ Python/problem007.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python3
+'''
+Problem 7 - Project Euler
+
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the 10 001st prime number?
+
+http://projecteuler.net/index.php?section=problems&id=7
+'''
+import math
+
+primes = []
+
+def isPrime(num):
+    if num < 2:
+        return False
+    elif num == 2:
+        primes.append(num)
+        return True
+    else:
+        maxnum = math.ceil(math.sqrt(num))
+        for p in filter(lambda x: x <= maxnum, primes):
+            if num % p == 0:
+                return False
+        else:
+            print(num)
+            primes.append(num)
+            return True
+
+if __name__ == '__main__':
+    num = 2
+    count = 0
+    while True:
+        if isPrime(num):
+            count += 1
+            if count == 10_001:
+                print(str(num)+' is the 10,001st prime number.')
+                break
+        num += 1
