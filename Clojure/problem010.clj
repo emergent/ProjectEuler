ceb76bde08fcce5d5a03f5052717eb717dc27392
emergent <yosikawa@gmail.com>
2018-08-17 01:25:25 +0900

    solved problem010 using Clojure

---
+++ Clojure/problem010.clj
@@ -0,0 +1,28 @@
+#! /usr/bin/env boot
+; Problem 10 - Project Euler
+; 
+; The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+; Find the sum of all the primes below two million.
+;
+; http://projecteuler.net/index.php?section=problems&id=10
+
+(defn sieve [n max ls]
+  (let [k (nth ls n)]
+    (if (> k max)
+      ls
+      (recur (+ n 1)
+             max
+             (filter #(or (= % k) (not= (mod % k) 0)) ls)))))
+
+(defn primes [x]
+  (cond
+    (<= x 1) '()
+    (=  x 2) '(2)
+    (>  x 2) (cons 2 (sieve 0 (Math/sqrt x) (range 3 (+ x 1) 2)))))
+
+(defn sumprimes [xmax]
+  ;(+ 2 (sumoddprimes 0 3 xmax)))
+  (reduce + (primes xmax)))
+
+(defn -main []
+  (println (sumprimes (bigint (* 2 (Math/pow 1000 2))))))
