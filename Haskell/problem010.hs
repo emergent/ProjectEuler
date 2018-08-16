0217e630cb5364cec088b08a937a51ddb874ebc9
emergent <yosikawa@gmail.com>
2018-08-17 01:59:33 +0900

    mod by hlint

--- Haskell/problem010.hs
+++ Haskell/problem010.hs
@@ -17,15 +17,15 @@ sieve n maxn ls =
             then ls
             else sieve (n + 1) maxn (filter f ls)
 
+sqfl :: Int -> Int
 sqfl = floor . sqrt . fromIntegral
 
 primes :: Int -> [Int]
 primes x
     | x <  2 = []
-    | x == 2 = 2 : []
-    | x >  2 = 2 : (sieve 0 (sqfl x) [3,5..x])
+    | x == 2 = [2]
+    | x >  2 = 2 : sieve 0 (sqfl x) [3,5..x]
 
 problem010 = sum . primes $ (2 * 1000 * 1000)
 
-main = do
-    print $ "problem010: answer " ++ show problem010
+main = print $ "problem010: answer " ++ show problem010
