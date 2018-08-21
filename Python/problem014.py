e4340507011b76000f604f03bbc269f7659f83e0
emergent <yosikawa@gmail.com>
2018-08-21 22:34:02 +0900

    tweak

--- Python/problem014.py
+++ Python/problem014.py
@@ -10,12 +10,9 @@ Using the rule above and starting with 13, we generate the following sequence:
 
 <div style="text-align:center;">13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1</div>
 It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
-
 Which starting number, under one million, produces the longest chain?
 
 <b>NOTE:</b> Once the chain starts the terms are allowed to go above one million.
-
-
 
 http://projecteuler.net/index.php?section=problems&id=014
 '''
