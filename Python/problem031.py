eaae67fb16c5801f2af07cc3be99f5a71f9b5c10
emergent <yosikawa@gmail.com>
2018-09-06 01:20:00 +0900

    deleted debug log

--- Python/problem031.py
+++ Python/problem031.py
@@ -5,13 +5,8 @@ http://projecteuler.net/index.php?section=problems&id=031
 '''
 def coin_patterns(rest, coins, pattern, ways):
     if len(coins) == 1 and coins[0] == 1:
-        #print(1, rest, coins, pattern, ways)
         pattern.append(rest)
-        #print(2, rest, coins, pattern, ways)
-
         ways.append(pattern)
-        #print(3, ways)
-        #print(4, pattern, coins, len(ways))
     else:
         n = rest // coins[0]
         for i in range(0, n+1):
@@ -22,5 +17,4 @@ def coin_patterns(rest, coins, pattern, ways):
 if __name__ == '__main__':
     coins = [200, 100, 50, 20, 10, 5, 2, 1]
     ways = coin_patterns(200, coins, [], [])
-    #print(ways)
     print(len(ways))
