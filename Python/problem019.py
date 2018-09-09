c3a82941e3b7a8a751472beee4fe7f86ee3e79e1
emergent <yosikawa@gmail.com>
2018-09-09 17:04:33 +0900

    solved problem019 in Python

---
+++ Python/problem019.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python3
+'''
+Problem 19 - Project Euler
+http://projecteuler.net/index.php?section=problems&id=019
+'''
+import math
+
+daysofmonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def count_sundays():
+    days1901to2000 = []
+    for year in range(1901, 2000+1):
+        if year % 400 == 0 or (year % 100 != 0 and year % 4 == 0):
+        #if year % 4 == 0:
+            dom_leap = daysofmonths[:]
+            dom_leap[1] = 29
+            #print(dom_leap)
+            days1901to2000.extend(dom_leap)
+        else:
+            days1901to2000.extend(daysofmonths)
+
+    #print(days1901to2000)
+    amountofdays = 0
+    sundays = 0
+    for days in days1901to2000:
+        if (amountofdays % 7 == 5): # 1 Jan 1901 was a Tuesday
+            sundays += 1 
+        amountofdays += days
+    return sundays
+
+if __name__ == '__main__':
+    print(count_sundays())
