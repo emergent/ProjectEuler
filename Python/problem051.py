7208260725caaf0a818804e602af339d3ab58935
emergent <yosikawa@gmail.com>
2018-10-01 23:01:52 +0900

    solved problem051 in Python

---
+++ Python/problem051.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+'''
+Problem 51 - Project Euler
+http://projecteuler.net/index.php?section=problems&id=051
+'''
+def samedigits(x,y):
+    return set(str(x)) == set(str(y))
+
+if __name__ == '__main__':
+    maxn = 6
+    x = 1
+    while True:
+        for n in range(2, maxn+1):
+            if not samedigits(x, x * n):
+                break
+        else:
+            print([x * i for i in range(1, maxn+1)])
+            break
+        
+        x += 1
