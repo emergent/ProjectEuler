6ae07a9dc9f7e141a13182a554b410d8bb7954c0
emergent <yosikawa@gmail.com>
2018-10-10 01:11:34 +0900

    solved problem016 in Clojure

---
+++ Clojure/problem016.clj
@@ -0,0 +1,21 @@
+#! /usr/bin/env clojure
+; Problem 16 - Project Euler
+; http://projecteuler.net/index.php?section=problems&id=016
+
+
+(defn power [a b]
+  (reduce *' (repeat b a)))
+
+(->>
+ (->
+  (power 2 1000)
+  (str)
+  (clojure.string/split #""))
+ (map #(Integer/parseInt %))
+ (reduce +)
+ (println))
+; (long)
+; (str)
+; (map int)
+; (sum)
+; (println))
