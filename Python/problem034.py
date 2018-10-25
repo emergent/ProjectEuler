f170db75b9a6f47a0e39dcf7ce915c2ab74966a7
emergent <yosikawa@gmail.com>
2018-10-26 01:31:06 +0900

    並列処理で少しだけ高速化

--- Python/problem034.py
+++ Python/problem034.py
@@ -4,11 +4,16 @@ Problem 34 - Project Euler
 http://projecteuler.net/index.php?section=problems&id=034
 '''
 from math import factorial
+from multiprocessing import Pool
+import numpy as np
 
 def digitfact(x):
-    return x == sum(map(lambda i: factorial(int(i)), list(str(x))))
+    return (x, x == sum(map(lambda i: factorial(int(i)), list(str(x)))))
 
 if __name__ == '__main__':
-    df = [x for x in range(3, 10000000) if digitfact(x)]
+    with Pool(8) as p:
+        df = p.map(digitfact, range(3, 10000000))
+    
+    df = list(map(lambda y: y[0], filter(lambda x: x[1], df)))
     print(df)
     print(sum(df))
