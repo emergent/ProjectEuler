5d74539955fa4597de2d131dbaa6a407cb217aed
emergent <yosikawa@gmail.com>
2018-10-28 18:27:45 +0900

    solved problem056 in Python

---
+++ Python/problem056.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+'''
+Problem 56 - Project Euler
+http://projecteuler.net/index.php?section=problems&id=056
+'''
+import math
+
+def digitalsum(x):
+    return sum(map(int, list(str(x))))
+
+if __name__ == '__main__':
+    print(max([digitalsum(x ** y) for x in range(100) for y in range(100)]))
