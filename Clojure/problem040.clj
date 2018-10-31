e676899d77317a89cd8eda6011ceae83c7e046f8
emergent <yosikawa@gmail.com>
2018-10-31 12:04:31 +0900

    tweak

--- Clojure/problem040.clj
+++ Clojure/problem040.clj
@@ -2,7 +2,7 @@
 ; Problem 40 - Project Euler
 ; http://projecteuler.net/index.php?section=problems&id=040
 
-(defn concate-integers [nmax]
+(defn concat-integers [nmax]
   (->> (range 0 nmax)
        (map str)
        (clojure.string/join "")))
@@ -10,7 +10,7 @@
 (defn ppp [s] (do (println s) s))
 (defn c2i [c] (- (int c) 48))
 
-(let [s (concate-integers 1000000)]
+(let [s (concat-integers 1000000)]
   (->> (list (nth s 1)
              (nth s 10)
              (nth s 100)
