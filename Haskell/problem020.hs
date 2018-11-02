e022122490b1542148f81f3276a2ed3410b6fe29
emergent <yosikawa@gmail.com>
2018-11-02 14:43:34 +0900

    solved problem020 in Haskell

---
+++ Haskell/problem020.hs
@@ -0,0 +1,16 @@
+{-
+Problem 20 - Project Euler
+http://projecteuler.net/index.php?section=problems&id=020
+-}
+
+factorial n
+    | n < 2 = 1
+    | otherwise = n * factorial (n - 1)
+
+problem020 = 
+            let
+                n = show $ factorial 100
+            in
+                sum (map (read . return) n :: [Int])
+
+main = putStrLn $ "problem020: answer " ++ show problem020
