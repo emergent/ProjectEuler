595017f91ff9a6abbc3faa178c9053f218f7933e
emergent <yosikawa@gmail.com>
2018-11-02 19:37:57 +0900

    simplify 'factorial' function

--- Haskell/problem020.hs
+++ Haskell/problem020.hs
@@ -3,14 +3,12 @@ Problem 20 - Project Euler
 http://projecteuler.net/index.php?section=problems&id=020
 -}
 
-factorial n
-    | n < 2 = 1
-    | otherwise = n * factorial (n - 1)
+factorial n = product [1..n]
 
 problem020 = 
             let
                 n = show $ factorial 100
             in
-                sum (map (read . return) n :: [Int])
+                sum (map (read .return)  n :: [Int])
 
 main = putStrLn $ "problem020: answer " ++ show problem020
