6eb6c90cab29ad0bf6d4a3d5da74ac090a0fbbe1
emergent <yosikawa@gmail.com>
2018-11-10 14:58:46 +0900

    add mathtools for Clojure

---
+++ Clojure/src/mathtools.clj
@@ -0,0 +1,29 @@
+(ns mathtools)
+
+(defn ppp [str]
+  (do (println str) str))
+
+(defn dec2list [x]
+  (map #(Integer/parseInt %)
+       (-> (str x)
+           (clojure.string/split #""))))
+
+(defn factorial [x]
+  (reduce *' (range 1 (inc x))))
+
+(defn power [a b]
+  (reduce *' (repeat b a)))
+
+(defn sieve [n max ls]
+  (let [k (nth ls n)]
+    (if (> k max)
+      ls
+      (recur (+ n 1)
+             max
+             (filter #(or (= % k) (not= (mod % k) 0)) ls)))))
+
+(defn primes [x]
+  (cond
+    (<= x 1) '()
+    (=  x 2) '(2)
+    (>  x 2) (cons 2 (sieve 0 (Math/sqrt x) (range 3 (inc x) 2)))))
