6a5de4245e8282ee2a27dfafb64f8bd5658088d3
emergent <yosikawa@gmail.com>
2018-11-12 15:22:00 +0900

    fixed solution according to the intention of the question

--- Python/problem006.py
+++ Python/problem006.py
@@ -3,7 +3,8 @@
 Problem 6 - Project Euler
 http://projecteuler.net/index.php?section=problems&id=6
 '''
+from itertools import combinations
 def diffs(xs):
-    return sum(xs)**2 - sum(x**2 for x in xs)
+    return 2 * sum(map(lambda t: t[0]*t[1], combinations(xs, 2)))
 
 print(diffs(range(1, 101)))
