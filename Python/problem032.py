742240828b2cb859ed8161711f1d95fdbb83261c
emergent <yosikawa@gmail.com>
2018-11-23 21:16:00 +0900

    problem029: improved logic in Python

--- Python/problem032.py
+++ Python/problem032.py
@@ -5,22 +5,18 @@ http://projecteuler.net/index.php?section=problems&id=032
 '''
 from itertools import permutations
 
-def pandigitalproducts(x):
-    sep = '|'
-    pat = ''.join(map(str,range(1,x+1)))+sep*2
-    perms = set(filter(lambda s: not(s[0] == sep or s[-1] == sep or sep*2 in s), 
-                map(''.join, permutations(pat))))
-    pps = [] # (multiplicand, multiplier, product)
-
-    for p in perms:
-        md, mr, pr = map(int, p.split(sep))
-        if md * mr == pr:
-            pps.append((md, mr, pr))
-
-    print(pps)    
-    return pps
-
+def pandigitalproducts(s):
+    prod = int(''.join(s[5:])) # 4-digits
+    for i in (1,2,3):
+        m1 = int(''.join(s[:i]))
+        m2 = int(''.join(s[i:5]))
+        if m1 * m2 == prod:
+            return (m1, m2, prod)
+    else:
+        return None
+        
 if __name__ == '__main__':
-    pp = pandigitalproducts(9)
+    pp = filter(lambda x: x is not None, 
+                [pandigitalproducts(n) for n in permutations("123456789")])
     # sum only once by using set()
-    print(sum(set(map(lambda x: x[2], pp))))
+    print(sum(set(map(lambda x: print(x) or x[2], pp))))
