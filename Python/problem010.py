6f74dc56a5ac04cf05449265a22d7f97a140c05c
emergent <yosikawa@gmail.com>
2018-12-01 23:41:37 +0900

    solved problem010 in Python

---
+++ Python/problem010.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+'''
+Problem 10 - Project Euler
+http://projecteuler.net/index.php?section=problems&id=10
+'''
+from mathtools import primes
+
+if __name__ == '__main__':
+    print(sum(primes(2_000_000)))
