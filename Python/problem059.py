46283c77df0867fc0dcda6519bee1335992e223d
emergent <yosikawa@gmail.com>
2018-12-12 16:42:10 +0900

    tweak

--- Python/problem059.py
+++ Python/problem059.py
@@ -7,17 +7,18 @@ from itertools import product
 
 if __name__ == '__main__':
     with open('data/p059_cipher.txt') as f:
-        asciis = list(map(int, f.read().strip().split(',')))
+        cipher = list(map(int, f.read().strip().split(',')))
 
-    lenasc = len(asciis)
+    lenasc = len(cipher)
     keylen = 3
     for c in product(range(ord('a'), ord('z')+1), repeat=keylen):
         cs = [c[i % keylen] for i in range(lenasc)]
-        decry_i = list(map(lambda x,y: x ^ y, asciis, cs))
+        decry_i = list(map(lambda x,y: x ^ y, cipher, cs))
 
-        if all([32 <= i and i <= 122 for i in decry_i]):
+        if all([32 <= i <= 122 for i in decry_i]):
             decry_s = ''.join(map(chr, decry_i))
             if 'the' in decry_s:
                 print(decry_s)
                 print('key: '+''.join(map(chr,c)))
                 print(sum(decry_i))
+                break
