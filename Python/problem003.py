821cc82d005b56bce42f678baae180354463cb6d
emergent <yosikawa@gmail.com>
2019-01-01 12:52:39 +0900

    fixed bug

--- Python/problem003.py
+++ Python/problem003.py
@@ -6,10 +6,18 @@ http://projecteuler.net/index.php?section=problems&id=003
 import math
 from mathtools import primes
 
+def maxprimefactor(x, ps):
+    if len(ps) == 0:
+        return x
+    else:
+        if x == ps[0]:
+            return x
+        elif x % ps[0] == 0:
+            qx = x // ps[0]
+            return maxprimefactor(qx, ps)
+        else:
+            return maxprimefactor(x, ps[1:])
+
 if __name__ == '__main__':
     N = 600851475143
-    ps = primes(int(math.sqrt(N))+1)
-    for i in ps[::-1]:
-        if N % i == 0:
-            print(i)
-            break
+    print(maxprimefactor(N, primes(int(math.sqrt(N))+1)))
