7deff6fa9270ff288333f811024627e3d995dc01
emergent <yosikawa@gmail.com>
2019-01-06 15:08:04 +0900

    solved problem030 in Rust

---
+++ Rust/src/bin/030.rs
@@ -0,0 +1,24 @@
+/// Problem 30 - Project Euler
+/// http://projecteuler.net/index.php?section=problems&id=30
+
+fn nthpower(x: i32, e: u32) -> bool {
+    let mut quot = x;
+    let mut sumpower = 0;
+    loop {
+        sumpower += (quot % 10).pow(e);
+
+        quot = quot / 10;
+        if quot == 0 {
+            break;
+        }
+    }
+
+    if sumpower == x { true } else { false }
+}
+
+fn main() {
+    let ans: i32 = (11..1000000)
+                .filter(|&x| nthpower(x, 5))
+                .sum();
+    println!("{}", ans);
+}
