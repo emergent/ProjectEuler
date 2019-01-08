a1c71ea154a4c6169896e064d7f4b19ca8c63245
emergent <yosikawa@gmail.com>
2019-01-08 12:22:39 +0900

    solved problem015 in C++

---
+++ C++/problem015.cpp
@@ -0,0 +1,18 @@
+/*
+ * Problem 15 - Project Euler
+ * http://projecteuler.net/index.php?section=problems&id=15
+ */
+#include <iostream>
+
+long comb(long n, long k) {
+    if (k == 1) {
+        return n;
+    } else {
+        return comb(n, k-1) * (n-k+1) / k;
+    }
+}
+
+int main(int argc, char **argv) {
+    std::cout << comb(40, 20) << std::endl;
+    return 0;
+}
