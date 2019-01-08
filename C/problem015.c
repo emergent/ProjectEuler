4d3ae12874478fff880061c23292b51c28283935
emergent <yosikawa@gmail.com>
2019-01-08 12:22:00 +0900

    tweak

--- C/problem015.c
+++ C/problem015.c
@@ -3,7 +3,8 @@
  * http://projecteuler.net/index.php?section=problems&id=015
  */
 #include <stdio.h>
-typedef long long llong;
+#include <limits.h>
+typedef long long int llong;
 
 llong comb(llong n, llong k) {
     if (k == 1) {
@@ -20,6 +21,11 @@ void problem015() {
     ans = comb(40, 20);
 
     printf("%s: answer = %lld\n", __FUNCTION__, ans);
+    printf("%lld\n", __LONG_LONG_MAX__);
+    printf("%lld\n", LLONG_MAX);
+    printf("%ld\n", __LONG_MAX__);
+    printf("%ld\n", LONG_MAX);
+    printf("%d\n", INT_MAX);
 }
 
 int main(int argc, char* argv[]) {
