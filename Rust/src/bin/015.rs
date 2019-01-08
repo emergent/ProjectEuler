2b9d03f62f5cfad825bc0a8c5a36245f837ae179
emergent <yosikawa@gmail.com>
2019-01-08 12:25:35 +0900

    solved problem015 in Rust

---
+++ Rust/src/bin/015.rs
@@ -0,0 +1,12 @@
+/// Problem 15 - Project Euler
+/// http://projecteuler.net/index.php?section=problems&id=15
+fn comb(n: i64, k: i64) -> i64 {
+    match k {
+        1 => n, 
+        _ => comb(n, k-1) * (n-k+1) / k,
+    }
+}
+
+fn main() {
+    println!("{}", comb(40,20));
+}
