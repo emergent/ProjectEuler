4d7ff669227629c34ee938c063c686ac4c079b53
emergent <yosikawa@gmail.com>
2019-01-08 11:55:24 +0900

    shorten code

--- Rust/src/bin/030.rs
+++ Rust/src/bin/030.rs
@@ -4,21 +4,16 @@
 fn nthpower(x: i32, e: u32) -> bool {
     let mut quot = x;
     let mut sumpower = 0;
-    loop {
+
+    while quot != 0 {
         sumpower += (quot % 10).pow(e);
-
         quot = quot / 10;
-        if quot == 0 {
-            break;
-        }
     }
-
+    
     if sumpower == x { true } else { false }
 }
 
 fn main() {
-    let ans: i32 = (11..1000000)
-                .filter(|&x| nthpower(x, 5))
-                .sum();
+    let ans: i32 = (11..1000000).filter(|&x| nthpower(x, 5)).sum();
     println!("{}", ans);
 }
