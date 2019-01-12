e96e0608e63a402962b3c11d341ab2009a4f3e86
emergent <yosikawa@gmail.com>
2019-01-13 02:05:30 +0900

    tweak

--- Rust/src/bin/031.rs
+++ Rust/src/bin/031.rs
@@ -3,17 +3,7 @@
 
 fn main() {
     let lim = 200 + 1;
-    /*
-    let mut pat = 0;
-    for a in (0..lim).step_by(100) {
-        for b in (0..(lim-a)).step_by(50) {
-            for c in (0..(lim-(a+b))).step_by(20) {
-                for d in (0..(lim-(a+b+c))).step_by(10) {
-                    for e in (0..(lim-(a+b+c+d))).step_by(5) {
-                        for _ in (0..(lim-(a+b+c+d+e))).step_by(2) {
-                            pat += 1; }}}}}}
-     */
-    let pat = (0..lim).step_by(100).map(|a|
+    let p = (0..lim).step_by(100).map(|a|
                 (0..(lim-a)).step_by(50).map(|b|
                     (0..(lim-(a+b))).step_by(20).map(|c|
                         (0..(lim-(a+b+c))).step_by(10).map(|d|
@@ -24,5 +14,5 @@ fn main() {
                     ).fold(0,|s,x| s+x)
                 ).fold(0,|s,x| s+x)
             ).fold(0,|s,x| s+x);
-    println!("{}", pat + 1);
+    println!("{}", p + 1);
 }
