35a973558ee1134afc586a7e703a466f0e56fcc4
emergent <yosikawa@gmail.com>
2019-01-13 18:40:00 +0900

    solved problem016 in Rust

---
+++ Rust/src/bin/016.rs
@@ -0,0 +1,25 @@
+/// Problem 16 - Project Euler
+/// http://projecteuler.net/index.php?section=problems&id=16
+extern crate num;
+
+use num::FromPrimitive;
+use num::bigint::BigInt;
+
+fn sumofdigitspower(x: i32, p: usize) -> i32 {
+    let xb: BigInt = FromPrimitive::from_i32(x).unwrap();
+    num::pow(xb, p)
+        .to_string()
+        .chars()
+        .map(|c| c as i32 -48)
+        .fold(0, |sum,x| sum + x)
+}
+
+#[test]
+fn test_sumof() {
+    assert_eq!(sumofdigitspower(2, 2), 4);
+    assert_eq!(sumofdigitspower(2, 10), 7);
+}
+
+fn main() {
+    println!("{}", sumofdigitspower(2, 1000));
+}
