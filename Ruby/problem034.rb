768c862ce18d044cd9e0b75449721bcbf0f2151c
emergent <yosikawa@gmail.com>
2019-01-14 22:53:03 +0900

    solved problem 034 in Ruby

---
+++ Ruby/problem034.rb
@@ -0,0 +1,10 @@
+#! /usr/bin/env ruby
+=begin
+Problem 34 - Project Euler
+http://projecteuler.net/index.php?section=problems&id=34
+=end
+puts (3..((1..9).inject(:*)*7)).select {|i|
+    i.to_s.chars.map {|c|
+        d = c.to_i; d > 0 ? (1..d).inject(:*) : 1
+    }.sum == i
+}.sum
