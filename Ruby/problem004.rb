2868f542eb18bb8326899a6390c8363d4e92f2f2
emergent <yosikawa@gmail.com>
2019-01-14 18:14:12 +0900

    tweak

--- Ruby/problem004.rb
+++ Ruby/problem004.rb
@@ -3,13 +3,14 @@
 Problem 4 - Project Euler
 http://projecteuler.net/index.php?section=problems&id=4
 =end
-
-def palindromic? x
-    x.to_s.reverse == x.to_s
+class Integer
+    def palindromic?
+        self.to_s.reverse == self.to_s
+    end
 end
 
 puts (100..999).to_a
         .combination(2)
         .map {|a| a[0] * a[1]}
-        .select {|x| palindromic?(x) }
+        .select {|x| x.palindromic? }
         .max
