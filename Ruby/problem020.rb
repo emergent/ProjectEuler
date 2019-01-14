35f99f7f20d8a832687229498ca61468def0e551
emergent <yosikawa@gmail.com>
2019-01-14 18:36:27 +0900

    simplify

--- Ruby/problem020.rb
+++ Ruby/problem020.rb
@@ -3,5 +3,4 @@
 Problem 20 - Project Euler
 http://projecteuler.net/index.php?section=problems&id=020
 =end
-fac = (1..100).inject(:*)
-puts fac.to_s.split('').inject(0) {|res,i| res += i.to_i }
+puts (1..100).inject(:*).to_s.chars.map {|c| c.to_i }.sum
