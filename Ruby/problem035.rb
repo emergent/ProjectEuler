ac85f1f40a94837e373338ecffce7e5c83baf51c
emergent <yosikawa@gmail.com>
2019-01-16 02:37:39 +0900

    use recursion

--- Ruby/problem035.rb
+++ Ruby/problem035.rb
@@ -10,34 +10,28 @@ def rotate x
     (xs[1..-1] + xs[0]).to_i
 end
 
-def rotate_primes arr
-    result = []
-    tmp = []
-    while !arr.empty?
-        x = arr.shift
-        xr = rotate(x)
-        if x == xr
-            result << xr
-            next
-        end
+def rotate_filter arr, res
+    return res if arr.empty?
 
-        tmp << x
-        while x != xr
-            if arr.include? xr
-                arr.delete xr
-                tmp << xr
-            else
-                tmp = []
-                break
-            end
-            xr = rotate(xr)
+    x = arr.shift
+    tmp = [x]
+    xr = rotate(x)
+
+    while x != xr
+        if arr.include? xr
+            arr.delete xr
+            tmp << xr
+        else
+            tmp = []
+            break
         end
-        result += tmp
-        tmp = []
+        xr = rotate(xr)
     end
-    result
+
+    rotate_filter arr, res + tmp
 end
 
-puts (rotate_primes(Prime.each(1_000_000).to_a
-    .select {|i| i.to_s =~ /^[^024568]+$/ && i > 10 }
-    ) + [2,3,5,7]).count
+puts (rotate_filter(
+        Prime.each(1_000_000).to_a.select {|i| i.to_s =~ /^[^024568]+$/ && i > 10 }, \
+        []) + [2,3,5,7]
+     ).count
