474dd71b424bf25aad3791f585d3a72b9c649c15
emergent <yosikawa@gmail.com>
2019-01-16 02:01:01 +0900

    skip some numbers

--- Ruby/problem036.rb
+++ Ruby/problem036.rb
@@ -3,7 +3,7 @@
 Problem 36 - Project Euler
 http://projecteuler.net/index.php?section=problems&id=36
 =end
-puts (1..(1_000_000-1))
+puts (1..(1_000_000-1)).step(2)
     .select {|x| xs = x.to_s; xs == xs.reverse }
     .select {|x| x2 = x.to_s(2); x2 == x2.reverse }
     .sum
