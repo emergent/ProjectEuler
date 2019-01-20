2f79f02e22ca8bf52bb7fed95202f6c1587b09b5
emergent <yosikawa@gmail.com>
2019-01-21 02:05:48 +0900

    add return type

--- TypeScript/problem001.ts
+++ TypeScript/problem001.ts
@@ -2,8 +2,9 @@
  * Problem 1 - Project Euler
  * http://projecteuler.net/index.php?section=problems&id=1
  */
-const summul = (n:number, x:number) => {
+const summul = (n:number, x:number): number => {
     return (x * ((n / x)|0) * ((1 + n / x)|0)) / 2;
 };
+
 const N = 999;
 console.log(summul(N, 3) + summul(N, 5) - summul(N, 15));
--- TypeScript/problem002.ts
+++ TypeScript/problem002.ts
@@ -2,7 +2,7 @@
  * Problem 2 - Project Euler
  * http://projecteuler.net/index.php?section=problems&id=2
  */
-const fibevensum = (fmax: number) => {
+const fibevensum = (fmax: number): number => {
     let a: number = 1;
     let b: number = 2;
     let sum: number = 0;
