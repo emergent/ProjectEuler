1a0e82c13065b8f1e763fa44fefd555cd31c3135
emergent <yosikawa@gmail.com>
2019-01-21 11:54:11 +0900

    use block to declair const N

--- TypeScript/problem001.ts
+++ TypeScript/problem001.ts
@@ -6,5 +6,7 @@ const summul = (n:number, x:number): number => {
     return (x * ((n / x)|0) * ((1 + n / x)|0)) / 2;
 };
 
-const N = 999;
-console.log(summul(N, 3) + summul(N, 5) - summul(N, 15));
+{
+    const N = 999;
+    console.log(summul(N, 3) + summul(N, 5) - summul(N, 15));
+}
