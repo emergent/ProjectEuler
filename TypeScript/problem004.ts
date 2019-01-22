cc25312feeaa2032c3a645a6489402a87a40b17d
emergent <yosikawa@gmail.com>
2019-01-22 18:20:44 +0900

    solved problem004 in TypeScript

---
+++ TypeScript/problem004.ts
@@ -0,0 +1,18 @@
+/*
+ * Problem 4 - Project Euler
+ * http://projecteuler.net/index.php?section=problems&id=4
+ */
+const is_parindromic = (x: number): boolean => {
+    return x === parseInt(x.toString().split('').reverse().join(''));
+};
+
+let nmax = 0;
+for (let a=100; a < 1000; a++) {
+    for (let b=100; b < 1000; b++) {
+        let n = a * b;
+        if (is_parindromic(n)) {
+            nmax = nmax > n ? nmax : n;
+        }
+    }
+}
+console.log(nmax);
