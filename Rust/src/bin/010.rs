2da99fab6cbb11ab7271aba66d20bf8bd8a6b175
emergent <yosikawa@gmail.com>
2019-01-27 00:23:14 +0900

    delete unnecessary print

--- Rust/src/bin/010.rs
+++ Rust/src/bin/010.rs
@@ -4,7 +4,6 @@ extern crate project_euler;
 use project_euler::primes;
 
 fn main() {
-    let ans: u64 = primes(2_000_000).iter().fold(0, |sum, &x| sum + x as u64);
-    println!("{}", primes(2_000_000).last().unwrap());
+    let ans = primes(2_000_000).iter().fold(0, |sum, &x| sum + x as u64);
     println!("{}", ans);
 }
