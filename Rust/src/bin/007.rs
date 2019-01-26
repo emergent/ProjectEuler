f0480212a057cad0f88d7ea250105c8e8cb0ba7c
emergent <yosikawa@gmail.com>
2019-01-27 02:46:08 +0900

    use u64 in is_prime

--- Rust/src/bin/007.rs
+++ Rust/src/bin/007.rs
@@ -2,13 +2,13 @@
 /// http://projecteuler.net/index.php?section=problems&id=7
 
 extern crate project_euler;
-use project_euler::is_prime_i64;
+use project_euler::is_prime;
 
 fn main() {
     let mut counter = 1; // includes '2'
     let mut var = 3;
     loop {
-        if is_prime_i64(var) {
+        if is_prime(var) {
             counter += 1;
             if counter == 10001 { break; }
         }
--- Rust/src/bin/027.rs
+++ Rust/src/bin/027.rs
@@ -1,12 +1,16 @@
 /// Problem 27 - Project Euler
 /// http://projecteuler.net/index.php?section=problems&id=27
 extern crate project_euler;
-use project_euler::is_prime_i32;
+use project_euler::is_prime;
 
-fn count_primes(a: i32, b: i32) -> i32 {
+fn count_primes(a: i64, b: i64) -> i64 {
     let mut n = 0;
-    while is_prime_i32(n * n + a * n + b) {
-        n += 1;
+    let mut y;
+    loop {
+        y = n * n + a * n + b;
+        if y > 0 && is_prime(y as u64) {
+            n += 1;
+        } else { break; }
     }
     n
 }
@@ -14,7 +18,7 @@ fn count_primes(a: i32, b: i32) -> i32 {
 fn main() {
     let mut ans = (0, 0, 0);
     for a in -999..1000 {
-        for b in (2..1000).filter(|&i| is_prime_i32(i)) {
+        for b in (2..1000).filter(|&i| is_prime(i as u64)) {
             let num_of_primes = count_primes(a, b);
             if ans.2 < num_of_primes {
                 ans = (a, b, num_of_primes);
