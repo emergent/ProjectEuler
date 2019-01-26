1723fcce03ee9a8a98099f6fdf6c7930080142b2
emergent <yosikawa@gmail.com>
2019-01-27 03:47:02 +0900

    improve performance of problem037 in Python

--- Python/problem037.py
+++ Python/problem037.py
@@ -3,39 +3,25 @@
 Problem 37 - Project Euler
 http://projecteuler.net/index.php?section=problems&id=037
 '''
-import math
-from operator import and_
-from functools import reduce
+from mathtools import isprime
 
-def isPrime(x):
-    if x == 2:
-        return True
-    elif x < 2 or x % 2 == 0:
-        return False
-    else:
-        for i in range(3, int(math.sqrt(x))+1):
-            if x % i == 0:
-                return False
-        else:
-            return True
-
-def isTruncatable(x):
-    a = [x]
-    x_s = str(x)
-    for i in range(1, len(x_s)):
-        a.append(int(x_s[i:]))
-        a.append(int(x_s[:-i]))
-    return reduce(and_, map(isPrime, sorted(a)))
+def is_truncatable(x):
+    xr = x // 10
+    d = 10
+    while xr > 0:
+        xl = x % d
+        if not (isprime(xr) and isprime(xl)):
+            return False
+        xr = xr // 10
+        d *= 10
+    return isprime(x)
 
 if __name__ == '__main__':
     a = []
     i = 11
-    while True:
-        if isTruncatable(i):
+    while len(a) < 11:
+        if is_truncatable(i):
             a.append(i)
-            print(a)
-            if len(a) == 11:
-                break
         i += 2
+    print(a)
     print(sum(a))
-        
