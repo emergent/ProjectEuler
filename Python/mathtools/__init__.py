ee3741fad2436792f99d9121c493fedcc926baf0
emergent <yosikawa@gmail.com>
2019-01-27 00:19:00 +0900

    update method `primes` using numpy

--- Python/mathtools/__init__.py
+++ Python/mathtools/__init__.py
@@ -2,6 +2,7 @@
 # https://projecteuler.net/
 
 import math
+import numpy as np
 
 def isprime(x):
     if x == 2:
@@ -11,23 +12,22 @@ def isprime(x):
     else:
         return all([x % i != 0 for i in range(3, int(x**0.5)+1, 2)])
 
-def _sieve(ps, k, nmax):
-    n = ps[k]
-    if n > nmax:
-        return ps
-    else:
-        return _sieve([x for x in ps if x % n != 0 or x == n], k+1, nmax)
-
 def primes(maxnum):
-    '''
-    this function returns the list from 2 to the prime number less than or equal to `maxnum`.
-    '''
     if maxnum < 2:
         return []
     elif maxnum == 2:
         return [2]
     else:
-        return [2] + _sieve(list(range(3, int(maxnum)+1, 2)), 0, int(math.sqrt(maxnum)))
+        ps = np.array([], dtype=np.int32)
+        target = np.array(range(2, maxnum+1), dtype=np.int32)
+        i = target[0]
+        while i * i <= maxnum:
+            ps = np.append(ps, i)
+            target = target[target%i!=0]
+            i = target[0]
+        ps = list(ps)
+        ps.extend(list(target))
+        return ps
 
 def rotate(x):
     '''
