24f627fe3d4d409ecf60f8210606751bbb2b9253
emergent <yosikawa@gmail.com>
2019-02-02 19:46:33 +0900

    use array instead of HashMap

--- Rust/src/bin/014.rs
+++ Rust/src/bin/014.rs
@@ -1,25 +1,25 @@
 /// Problem 14 - Project Euler
 /// http://projecteuler.net/index.php?section=problems&id=14
-use std::collections::HashMap;
+const N: u64 = 1_000_000;
 
-fn chain(n: u64, cache: &mut HashMap<u64, u64>) -> u64 {
-    match cache.get(&n) {
-        Some(&i) => i,
-        None => {
-            let collaz = match n {
-                1 => 1,
-                n if n % 2 == 0 => 1 + chain(n / 2, cache),
-                n => 1 + chain(3 * n + 1, cache),
-            };
-            cache.insert(n, collaz);
-            collaz
-        },
+fn chain(n: u64, cache: &mut [u64]) -> u64 {
+    if n < N && cache[n as usize] > 0 {
+        return cache[n as usize];
     }
+
+    let collaz = match n {
+        1 => 1,
+        n if n % 2 == 0 => 1 + chain(n / 2, cache),
+        n => 1 + chain(3 * n + 1, cache),
+    };
+    if n < N {
+        cache[n as usize] = collaz;
+    }
+    collaz
 }
 
 fn main() {
-    //let mut cache = HashMap::<u64,u64>::new();
-    let mut cache = HashMap::<u64,u64>::with_capacity(1024 * 1024);
+    let mut cache = [0; N as usize];
 
     let mut maxn = 0;
     let mut maxi = 0;
