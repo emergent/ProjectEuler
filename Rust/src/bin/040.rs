df8c9f1ad39cb8c2632046f299ebb81092510944
emergent <yosikawa@gmail.com>
2019-02-03 21:13:12 +0900

    solved problem040 in Rust

---
+++ Rust/src/bin/040.rs
@@ -0,0 +1,21 @@
+/// Problem 40 - Project Euler
+/// http://projecteuler.net/index.php?section=problems&id=40
+
+fn main() {
+    let mut numv = Vec::new();
+    let mut idx = 1;
+    let mut ans = 1;
+
+    for i in 1.. {
+        for c in i.to_string().chars() {
+            numv.push(c as i32 - 48);
+        }
+
+        if numv.len() > idx {
+            ans *= numv[idx-1];
+            idx *= 10;
+        }
+        if idx > 1000000 { break; }
+    }
+    println!("{}", ans);
+}
