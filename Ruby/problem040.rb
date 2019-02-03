671510ea0188455657f6407cd827cfbd49a99427
emergent <yosikawa@gmail.com>
2019-02-03 21:29:44 +0900

    improve performance

--- Ruby/problem040.rb
+++ Ruby/problem040.rb
@@ -3,12 +3,10 @@
 Problem 40 - Project Euler
 http://projecteuler.net/index.php?section=problems&id=40
 =end
+indice = (0..6).map {|i| 10**i }
 puts (1..).lazy
-          .flat_map {|i| i.to_s.chars }
+          .flat_map {|i| i.to_s.chars.map(&:to_i) }
           .first(1000000)
           .select
-          .with_index {|c, idx|
-            (1..6).map {|i| 10**i }
-                  .to_a
-                  .include? idx + 1
-          }.map(&:to_i).inject(:*)
+          .with_index {|_, idx| indice.include? idx + 1}
+          .inject(:*)
