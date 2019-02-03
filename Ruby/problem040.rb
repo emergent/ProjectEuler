b84e3e48bbbb111bc1f9dc6d0d2924c43e3953f1
emergent <yosikawa@gmail.com>
2019-02-03 22:17:29 +0900

    improve again

--- Ruby/problem040.rb
+++ Ruby/problem040.rb
@@ -3,10 +3,7 @@
 Problem 40 - Project Euler
 http://projecteuler.net/index.php?section=problems&id=40
 =end
-indice = (0..6).map {|i| 10**i }
-puts (1..).lazy
+ns = (1..).lazy
           .flat_map {|i| i.to_s.chars.map(&:to_i) }
           .first(1000000)
-          .select
-          .with_index {|_, idx| indice.include? idx + 1}
-          .inject(:*)
+puts (0..6).map {|i| ns[10**i-1] }.inject(:*)
