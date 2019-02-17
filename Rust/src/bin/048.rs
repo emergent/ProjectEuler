ca2ef84a0e165915abcae3066f7ac5df34c109a2
emergent <yosikawa@gmail.com>
2019-02-17 21:33:59 +0900

    solved problem048 in Rust

---
+++ Rust/src/bin/048.rs
@@ -0,0 +1,18 @@
+/// Problem 48 - Project Euler
+/// http://projecteuler.net/index.php?section=problems&id=48
+fn pow_last10d(n: u64, k: u64) -> u64 {
+    let mut ret: u64 = 1;
+    for _ in 0..k {
+        ret = ret * n % 10_000_000_000;
+    }
+    ret
+}
+
+fn main() {
+    let mut sum: u64 = 0;
+    for i in 1 ..= 1000 {
+        sum += pow_last10d(i, i);
+        sum %= 10_000_000_000;
+    }
+    println!("{}", sum);
+}
