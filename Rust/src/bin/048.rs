4b826a46b8cb0faf63e946a3cbcc17ac0d74e684
emergent <yosikawa@gmail.com>
2019-02-17 21:37:19 +0900

    define 10^10 as const

--- Rust/src/bin/048.rs
+++ Rust/src/bin/048.rs
@@ -1,9 +1,11 @@
 /// Problem 48 - Project Euler
 /// http://projecteuler.net/index.php?section=problems&id=48
+const N10: u64 = 10_000_000_000;
+
 fn pow_last10d(n: u64, k: u64) -> u64 {
     let mut ret: u64 = 1;
     for _ in 0..k {
-        ret = ret * n % 10_000_000_000;
+        ret = ret * n % N10;
     }
     ret
 }
@@ -12,7 +14,7 @@ fn main() {
     let mut sum: u64 = 0;
     for i in 1 ..= 1000 {
         sum += pow_last10d(i, i);
-        sum %= 10_000_000_000;
+        sum %= N10;
     }
     println!("{}", sum);
 }
