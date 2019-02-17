3e5d1fcccbceb659a353acc9d964e00cb704a205
emergent <yosikawa@gmail.com>
2019-02-17 18:35:37 +0900

    tweak

--- Python/problem044.py
+++ Python/problem044.py
@@ -8,9 +8,7 @@ import math
 def is_penta(y):
     d = 1 + 24 * y
     d_sq = int(math.sqrt(d))
-    if d_sq * d_sq == d and (d_sq + 1) % 6 == 0:
-        return True
-    return False
+    return d_sq * d_sq == d and (d_sq + 1) % 6 == 0
 
 def penta(n):
     return int(n * (3 * n - 1) / 2)
@@ -18,11 +16,14 @@ def penta(n):
 if __name__ == '__main__':
     k = 2
     while True:
-        pentas = [(penta(k), penta(j)) for j in range(1, k)
-                   if is_penta(penta(k)+penta(j)) and
-                      is_penta(penta(k)-penta(j))]
+        pk = penta(k)
+        pentas = [(pk, pj) for j in range(1, k)
+                    for pj in [penta(j)]
+                    if is_penta(pk+pj) and is_penta(pk-pj)]
+
         if len(pentas) > 0:
-            print(pentas[0])
-            print('D = {}'.format(abs(pentas[0][1]-pentas[0][0])))
+            pk, pj = pentas[0]
+            print(pk, pj)
+            print('D = {}'.format(abs(pk-pj)))
             break
         k += 1
