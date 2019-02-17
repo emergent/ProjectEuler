82e0fe300463dc2a80546a643dd4a130eb00b6e9
emergent <yosikawa@gmail.com>
2019-02-18 00:59:10 +0900

    tweak

--- Rust/src/bin/044.rs
+++ Rust/src/bin/044.rs
@@ -5,10 +5,7 @@ fn is_penta(y: i32) -> bool {
     let d = 1 + 24 * y;
     let d_sq = (d as f64).sqrt() as i32;
 
-    if d_sq * d_sq == d && (d_sq + 1) % 6 == 0 {
-        return true;
-    }
-    false
+    d_sq * d_sq == d && (d_sq + 1) % 6 == 0
 }
 
 fn penta(n: i32) -> i32 {
@@ -31,21 +28,10 @@ fn main() {
 
 #[test]
 fn test_is_penta() {
-    assert_eq!(is_penta(1), true);
-    assert_eq!(is_penta(5), true);
-    assert_eq!(is_penta(12), true);
-    assert_eq!(is_penta(22), true);
-    assert_eq!(is_penta(35), true);
-    assert_eq!(is_penta(51), true);
-    assert_eq!(is_penta(70), true);
-    assert_eq!(is_penta(92), true);
-    assert_eq!(is_penta(117), true);
-
-    assert_eq!(is_penta(2), false);
-    assert_eq!(is_penta(4), false);
-    assert_eq!(is_penta(10), false);
-    assert_eq!(is_penta(23), false);
-    assert_eq!(is_penta(40), false);
-    assert_eq!(is_penta(52), false);
-    assert_eq!(is_penta(100), false);
+    for i in 1 .. 120 {
+        match i {
+            1|5|12|22|35|51|70|92|117 => assert_eq!(is_penta(i), true),
+            _ => assert_eq!(is_penta(i), false),
+        }
+    }
 }
--- Rust/src/bin/048.rs
+++ Rust/src/bin/048.rs
@@ -4,7 +4,7 @@ const N10: u64 = 10_000_000_000;
 
 fn pow_last10d(n: u64, k: u64) -> u64 {
     let mut ret: u64 = 1;
-    for _ in 0..k {
+    for _ in 0 .. k {
         ret = ret * n % N10;
     }
     ret
