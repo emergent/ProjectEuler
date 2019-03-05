ec551f361c2a1abe98d88d36b04567aaac4b5337
emergent <yosikawa@gmail.com>
2019-03-06 02:46:04 +0900

    solved problem041 in Ruby

---
+++ Ruby/problem041.rb
@@ -0,0 +1,9 @@
+#! /usr/bin/env ruby
+=begin
+Problem 41 - Project Euler
+http://projecteuler.net/index.php?section=problems&id=41
+=end
+require 'prime'
+puts (1..7).to_a.permutation.lazy.map {|p|
+    p.map(&:to_s).join.to_i
+}.sort.reverse.select(&:prime?).first
