d86b1ef0f42ea5cd851bec37f31a1b62f7e274c9
emergent <yosikawa@gmail.com>
2019-03-24 22:04:40 +0900

    solved problem024 in Rust

---
+++ Rust/src/bin/024.rs
@@ -0,0 +1,10 @@
+/// Problem 24 - Project Euler
+/// http://projecteuler.net/index.php?section=problems&id=24
+extern crate project_euler;
+use project_euler::itertools::permutations;
+
+fn main() {
+    let lexperm = permutations(vec!['0','1','2','3','4','5','6','7','8','9'], 10);
+    let ans = lexperm[1000000-1].iter().collect::<String>().parse::<u32>().unwrap();
+    println!("{}", ans);
+}
