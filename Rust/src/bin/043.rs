8f8117d0118b74f643962b5a2d89f396422ce16f
emergent <yosikawa@gmail.com>
2019-04-03 15:30:55 +0900

    solved problem043 in Rust

---
+++ Rust/src/bin/043.rs
@@ -0,0 +1,42 @@
+/// Problem 43 - Project Euler
+/// http://projecteuler.net/index.php?section=problems&id=43
+extern crate project_euler;
+use project_euler::itertools::permutations;
+
+fn is_divisible(subnum: u64, d: u64) -> bool {
+    subnum % d == 0
+}
+
+fn all_divisible(pv: &Vec<char>, denomi: &Vec<u64>) -> bool {
+    for (i, d) in denomi.iter().enumerate() {
+        let n = subnum(pv, i+1, 3);
+        if !is_divisible(n, *d) {
+            return false;
+        }
+    }
+    true
+}
+
+fn subnum(xv: &Vec<char>, n: usize, digits_len: usize) -> u64 {
+    xv[n..n+digits_len]
+        .iter()
+        .collect::<String>()
+        .parse::<u64>()
+        .unwrap()
+}
+
+fn main() {
+    let denomi = vec![2, 3, 5, 7, 11, 13, 17];
+    let perm_in = "1234567890".chars().collect::<Vec<char>>();
+    let perm = permutations(&perm_in, perm_in.len() as u32);
+
+    let mut ansv: Vec<u64> = Vec::new();
+    for p in perm {
+        if all_divisible(&p, &denomi) {
+            let num = p.iter().collect::<String>().parse::<u64>().unwrap();
+            ansv.push(num);
+        }
+    }
+    let ans = ansv.iter().sum::<u64>();
+    println!("{}", ans);
+}
