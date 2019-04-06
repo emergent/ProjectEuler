b1dec77e3f5547656e1e62bfe435a4ff96d98577
emergent <yosikawa@gmail.com>
2019-04-07 00:44:44 +0900

    solved problem030 in Kotlin

---
+++ Kotlin/problem030.kt
@@ -0,0 +1,20 @@
+// Problem 30 - Project Euler
+// http://projecteuler.net/index.php?section=problems&id=30
+fun Int.nth_power(n: Int): Int {
+    var x = 1
+    repeat(n) { x *= this }
+    return x
+}
+
+fun main(args: Array<String>) {
+    val sum = (2..354294)
+            .map { x ->
+                Pair(x, x.toString()
+                        .map { c -> (c.toInt() - 48).nth_power(5) }
+                        .sum())
+            }
+            .filter { it.first == it.second }
+            .map { it.first }
+            .sum()
+    println(sum)
+}
