60149f759f2a1c6394994f2ba42f9671af16689b
emergent <yosikawa@gmail.com>
2019-04-06 16:02:09 +0900

    solved problem046 in Rust

---
+++ Rust/src/bin/046.rs
@@ -0,0 +1,33 @@
+/// Problem 46 - Project Euler
+/// http://projecteuler.net/index.php?section=problems&id=046
+extern crate project_euler;
+use project_euler::prime::is_prime;
+
+fn goldbach(x: u32) -> bool {
+    for i in 1 .. {
+        let twice_sq = i * i * 2;
+        if x > twice_sq + 2 {
+            if is_prime((x - twice_sq).into()) {
+                return true;
+            } else {
+                continue;
+            }
+        } else {
+            break;
+        }
+    }
+    false
+}
+
+fn main() {
+    for i in (9 ..).step_by(2) {
+        if is_prime(i.into()) {
+            continue;
+        } else {
+            if !goldbach(i) {
+                println!("{}", i);
+                break;
+            }
+        }
+    }
+}
