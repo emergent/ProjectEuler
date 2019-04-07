c18b673d4ab6ce4dce600040baee6269b56464ef
emergent <yosikawa@gmail.com>
2019-04-07 19:17:24 +0900

    tweak

--- Rust/src/bin/012.rs
+++ Rust/src/bin/012.rs
@@ -1,7 +1,7 @@
 /// Problem 12 - Project Euler
 /// http://projecteuler.net/index.php?section=problems&id=12
-fn triangle(i: i32) -> i32 {
-    (1..(i+1)).sum()
+fn triangle(n: i32) -> i32 {
+    n * (n + 1) / 2
 }
 
 fn divisors(x: i32) -> i32 {
