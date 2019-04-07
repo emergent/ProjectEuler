6f6bf5e05f37f6ff09fc7c6448a8b59c43ab3daa
emergent <yosikawa@gmail.com>
2019-04-08 03:13:32 +0900

    solved problem034 in Kotlin

---
+++ Kotlin/problem034.kt
@@ -0,0 +1,16 @@
+// Problem 34 - Project Euler
+// http://projecteuler.net/index.php?section=problems&id=34
+fun Int.fac() = when {
+    this <= 1 -> 1
+    else -> (1..this).fold(1) { acc, p -> acc * p }
+}
+
+fun main(args: Array<String>) {
+    println((10..2540160) // 9! * 7 = 2540160
+            .filter {
+                it == it.toString()
+                        .map { c -> (c.toInt() - 48).fac() }
+                        .sum()
+            }
+            .sum())
+}
