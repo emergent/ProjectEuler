d612307e8b82a18b98819276c2b3662c2bd47969
emergent <yosikawa@gmail.com>
2019-04-07 15:59:24 +0900

    solved problem047 in Rust

---
+++ Rust/src/bin/047.rs
@@ -0,0 +1,44 @@
+/// Problem 47 - Project Euler
+/// http://projecteuler.net/index.php?section=problems&id=47
+extern crate project_euler;
+use project_euler::prime::primes;
+use std::collections::HashSet;
+
+const CONSECUTIVE_NUMBERS_LEN: usize = 4;
+
+fn prime_factors(x: u32, ps: &Vec<u32>) -> Vec<u32> {
+    let mut v = Vec::new();
+
+    let mut dividend = x;
+    for &i in ps {
+        while dividend % i == 0 {
+            dividend = dividend / i;
+            v.push(i);
+        }
+        if dividend == 1 {
+            break;
+        }
+    }
+    v
+}
+
+fn main() {
+    let primes = primes(500_000);
+    let mut consecutives_count = 0;
+
+    for i in 2 .. {
+        let pf = prime_factors(i, &primes);
+        let pf_kinds = pf.iter().collect::<HashSet<_>>().len();
+
+        if pf_kinds == CONSECUTIVE_NUMBERS_LEN {
+            consecutives_count += 1;
+        } else {
+            consecutives_count = 0;
+        }
+
+        if consecutives_count == CONSECUTIVE_NUMBERS_LEN {
+            println!("{}", i - 3);
+            break;
+        }
+    }
+}
