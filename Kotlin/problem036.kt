dda09d9b83331c9ff13ab0f001f0548580623090
emergent <yosikawa@gmail.com>
2019-04-08 03:27:11 +0900

    solved problem036 in Kotlin

---
+++ Kotlin/problem036.kt
@@ -0,0 +1,11 @@
+// Problem 36 - Project Euler
+// http://projecteuler.net/index.php?section=problems&id=36
+fun Int.is_pal(base: Int): Boolean {
+    return this.toString(base).reversed() == this.toString(base)
+}
+
+fun main(args: Array<String>) {
+    println((1..1_000_000 step 2)
+            .filter { it.is_pal(2) && it.is_pal(10) }
+            .sum())
+}
