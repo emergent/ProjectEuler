f6f9c49dc43195e3d96954defbc87fcf569793ee
emergent <yosikawa@gmail.com>
2019-04-07 21:18:04 +0900

    solved problem021 in Kotlin

---
+++ Kotlin/problem021.kt
@@ -0,0 +1,30 @@
+// Problem 21 - Project Euler
+// http://projecteuler.net/index.php?section=problems&id=21
+import kotlin.math.*
+
+fun divisors(x: Int): List<Int> {
+    val lim = ceil(sqrt(x.toDouble())).toInt()
+    return (1..lim)
+            .map { n ->
+                if (x % n == 0) listOf(n, x/n) else null
+            }
+            .filterNotNull()
+            .flatten()
+            .distinct()
+            .toList()
+}
+
+fun d(x: Int) = divisors(x).sum() - x
+
+fun amicable_pair(x: Int): Int? {
+    val dx = d(x)
+    return if (d(dx) == x && dx != x) dx else null
+}
+
+fun main(args: Array<String>) {
+    val ans = (1..10000-1).map { amicable_pair(it) }
+            .filterNotNull()
+            .filter { it < 10000 }
+            .sum()
+    println(ans)
+}
