43a494d80b9794b5725e41db36ae41e4e0c2cfe8
emergent <yosikawa@gmail.com>
2019-04-07 19:11:26 +0900

    tweak

--- Rust/src/bin/006.rs
+++ Rust/src/bin/006.rs
@@ -1,9 +1,8 @@
 /// Problem 6 - Project Euler
 /// http://projecteuler.net/index.php?section=problems&id=6
-
 fn main() {
     let limit = 100;
-    let sumsq = ((1 + limit) * limit / 2 as i32).pow(2);
-    let sqsum = (1..(limit+1)).fold(0, |sum, a| sum + a * a);
+    let sumsq: i32 = ((1 + limit) * limit / 2 as i32).pow(2);
+    let sqsum: i32 = (1..=limit).map(|i| i * i).sum();
     println!("{}", sumsq - sqsum);
 }
