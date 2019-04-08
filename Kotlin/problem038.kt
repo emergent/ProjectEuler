40bd95592b5b5073501185e862391adda857c72f
emergent <yosikawa@gmail.com>
2019-04-09 02:54:15 +0900

    solved problem038 in Kotlin

---
+++ Kotlin/problem038.kt
@@ -0,0 +1,15 @@
+// Problem 38 - Project Euler
+// http://projecteuler.net/index.php?section=problems&id=38
+fun isPandigital9digits(x: Int): Boolean {
+    return (x.toString()+"0").toSet().count() == 10
+}
+
+fun main(args: Array<String>) {
+    val maxp = (9183..9999)
+            .map { p ->
+                (p.toString() + (p * 2).toString()).toInt()
+            }
+            .filter { isPandigital9digits(it) }
+            .max()
+    println(maxp)
+}
