a6de05f71f887185908580d22012f355a9597281
emergent <yosikawa@gmail.com>
2019-04-14 00:15:46 +0900

    improve performance

--- Rust/src/bin/050.rs
+++ Rust/src/bin/050.rs
@@ -1,17 +1,27 @@
 /// Problem 50 - Project Euler
 /// http://projecteuler.net/index.php?section=problems&id=50
 extern crate project_euler;
-use project_euler::prime::{primes, is_prime};
+use project_euler::prime::primes;
 
 fn main() {
-    let ps = primes(100_000);
+    let lim = 1_000_000;
+    let ps = primes(lim);
     let len = ps.len();
 
-    'outer: for i in (0..len).rev() {
+    let mut maxlen = 0;
+    let mut sumps = 0;
+    for (i, p) in ps.iter().enumerate() {
+        sumps += p;
+        if sumps > lim {
+            maxlen = i+1;
+            break;
+        }
+    }
+
+    'outer: for i in (0..maxlen).rev() {
         for j in (0..len-i).rev() {
             let sum = ps[j..j+i].iter().sum::<u32>();
-            //if sum < 1000_000 && ps.contains(&sum) {
-            if sum < 1000_000 && is_prime(sum.into()) {
+            if sum < lim && ps.contains(&sum) {
                 println!("{}, {}, {}", i, j, sum);
                 break 'outer
             }
