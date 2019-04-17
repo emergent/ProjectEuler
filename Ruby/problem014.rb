62200fee3fbb0b5e0a43f5de92bad72831c99106
emergent <yosikawa@gmail.com>
2019-04-17 10:45:04 +0900

    solved problem014 in Ruby

---
+++ Ruby/problem014.rb
@@ -0,0 +1,33 @@
+#! /usr/bin/env ruby
+=begin
+Problem 14 - Project Euler
+http://projecteuler.net/index.php?section=problems&id=14
+=end
+$cache = {} #Array.new(2_000_000, 0)
+
+def chain n
+    if $cache[n]
+        return $cache[n]
+    elsif n == 1
+        return 1
+    end
+
+    collaz = if n.even?
+        1 + chain(n / 2)
+    else
+        1 + chain(3 * n + 1)
+    end
+    $cache[n] = collaz
+    collaz
+end
+
+maxlen = 0
+maxval = 0
+for i in 1..1_000_000
+    c = chain i
+    if c > maxlen
+        maxlen = c
+        maxval = i
+    end
+end
+puts maxlen, maxval
