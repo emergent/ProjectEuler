80803c8e8fea105cc70c8bda4982f786140461ed
emergent <yosikawa@gmail.com>
2019-04-30 00:18:40 +0900

    solved problem005 in TypeScript

---
+++ TypeScript/problem005.ts
@@ -0,0 +1,18 @@
+/*
+ * Problem 5 - Project Euler
+ * http://projecteuler.net/index.php?section=problems&id=5
+ */
+const gcd = (x: number, y: number): number => {
+    return x === 0 ? y : gcd(y % x, x);
+};
+
+const lcm = (x: number, y: number): number => {
+    return (x * y) / gcd(x, y);
+};
+
+const reducer = (a: number, x: number): number => lcm(a, x);
+console.log(
+    Array.from(Array(20).keys())
+        .map(x => ++x)
+        .reduce(lcm)
+);
