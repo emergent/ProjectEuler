f48cc29b1f44392ff3e6f7df3effb9f398e84110
emergent <yosikawa@gmail.com>
2019-04-30 00:26:43 +0900

    solved problem006 in TypeScript

---
+++ TypeScript/problem006.ts
@@ -0,0 +1,11 @@
+/*
+ * Problem 6 - Project Euler
+ * http://projecteuler.net/index.php?section=problems&id=6
+ */
+let sqsum = 0;
+let sumsq = 0;
+for (let i = 1; i <= 100; i++) {
+    sqsum += i * i;
+    sumsq += i;
+}
+console.log(sumsq * sumsq - sqsum);
