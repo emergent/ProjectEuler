b863af570fc87291ab031079338ce28e5af123bc
emergent <yosikawa@gmail.com>
2019-05-13 11:51:50 +0900

    buf fix

--- Ruby/problem043.rb
+++ Ruby/problem043.rb
@@ -1,16 +1,13 @@
 #! /usr/bin/env ruby
-=begin
-Problem 43 - Project Euler
-http://projecteuler.net/index.php?section=problems&id=43
-=end
+# Problem 43 - Project Euler
+# http://projecteuler.net/index.php?section=problems&id=43
 
 def all_divisible(s)
   [2, 3, 5, 7, 11, 13, 17].lazy.map.with_index do |x, idx|
-    s[idx + 1, 3].to_i % x == 0
+    (s[idx + 1, 3].to_i % x).zero?
   end.all?
 end
 
 puts ('0'..'9').to_a.permutation.map(&:join).select { |sa|
        all_divisible(sa)
-     }.map(&:to_i)
-       .sum
+     }.map(&:to_i).sum
