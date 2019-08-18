8ee429d1cdbcba29cb829c1a7d7e61a7454d2524
emergent <yosikawa@gmail.com>
2019-08-18 17:57:50 +0900

    solved problem052 in Kotlin

---
+++ Kotlin/problem052.kt
@@ -0,0 +1,24 @@
+// Problem 52 - Project Euler
+// http://projecteuler.net/index.php?section=problems&id=52
+fun main(args: Array<String>) {
+    var i = 100000
+    while (!hasSameDigits(i)) i++
+    println(i)
+}
+
+fun hasSameDigits(x: Int): Boolean {
+    val dset = digitSet(x)
+    if (dset.size < x.toString().length) {
+        return false
+    }
+    for (i in 2..6) {
+        if (dset != digitSet(x * i)) {
+            return false
+        }
+    }
+    return true
+}
+
+fun digitSet(x: Int): Set<Int> {
+    return x.toString().map { it.toInt() - 48 }.toSet()
+}
