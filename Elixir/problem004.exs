28dbc5fd7354541c3b90e8c6ee033226d1257370
emergent <yosikawa@gmail.com>
2019-10-28 18:19:03 +0900

    solved problem004 in Elixir

---
+++ Elixir/problem004.exs
@@ -0,0 +1,27 @@
+#! /usr/bin/env ruby
+# Problem 4 - Project Euler
+# http://projecteuler.net/index.php?section=problems&id=4
+defmodule P004 do
+  def is_palindromic(x) do
+    rev = x
+          |> Integer.to_string
+          |> String.reverse
+          |> String.to_integer
+    x == rev
+  end
+
+  def solve(x) do
+    100..x
+    |> Stream.flat_map(
+         fn i ->
+           i..x
+           |> Stream.map(&(&1 * i))
+         end
+       )
+    |> Stream.uniq
+    |> Stream.filter(&is_palindromic/1)
+    |> Enum.max
+  end
+end
+
+IO.puts P004.solve(999)
