15266b0aa05e55447be0d9246ab573ea0ae0cd5c
emergent <yosikawa@gmail.com>
2019-10-31 21:34:11 +0900

    solved problem005 in Elixir

---
+++ Elixir/problem005.exs
@@ -0,0 +1,18 @@
+#! /usr/bin/env ruby
+# Problem 5 - Project Euler
+# http://projecteuler.net/index.php?section=problems&id=5
+defmodule P005 do
+  def gcd(a, 0), do: a
+  def gcd(0, b), do: b
+  def gcd(a, b), do: gcd(b, rem(a, b))
+
+  def lcm(0, 0), do: 0
+  def lcm(a, b), do: div(a * b, gcd(a, b))
+
+  def solve() do
+    1..20
+    |> Enum.reduce(10, &lcm/2)
+  end
+end
+
+IO.puts P005.solve()
