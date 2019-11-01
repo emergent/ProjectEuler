dc4b07ac749d6b785ee9efca2eabcd32f645ec5a
emergent <yosikawa@gmail.com>
2019-11-01 14:03:41 +0900

    solved problem006 in Elixir

---
+++ Elixir/problem006.exs
@@ -0,0 +1,14 @@
+#! /usr/bin/env ruby
+# Problem 6 - Project Euler
+# http://projecteuler.net/index.php?section=problems&id=6
+defmodule P006 do
+  def solve() do
+    a = 1..100
+        |> Enum.reduce(fn (x, acc) -> x * x + acc end)
+    b = 1..100
+        |> Enum.reduce(&+/2)
+    (b * b) - a
+  end
+end
+
+IO.puts P006.solve()
