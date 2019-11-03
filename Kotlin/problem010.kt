aa16d80e7dd3b132cfbc7d123e45174558ab04ac
emergent <yosikawa@gmail.com>
2019-11-04 01:03:08 +0900

    update `primes` function

--- Kotlin/problem010.kt
+++ Kotlin/problem010.kt
@@ -2,27 +2,22 @@
 // http://projecteuler.net/index.php?section=problems&id=10
 import kotlin.math.*
 
-fun primes(xmax: Int): List<Int> {
-    val lim = ceil(sqrt(xmax.toDouble())).toInt()
-    val ps3_ = (3..xmax step 2)
-            .filter { x ->
-                var ret = true
-                loop@ for (i in 3..lim) {
-                    when {
-                        x <= i -> {
-                            ret = true
-                            break@loop
-                        }
-                        x % i == 0 -> {
-                            ret = false
-                            break@loop
-                        }
-                        else -> continue@loop
-                    }
-                }
-                ret
+fun primes(n: Int): List<Int> {
+    val is_p = BooleanArray(n+1, {true})
+    val ps: MutableList<Int> = mutableListOf()
+    if (n >= 2) {
+        for (i in 2..n){
+            if (is_p[i]) {
+                ps.add(i)
             }
-    return listOf(2).plus(ps3_)
+            if (i * i <= n) {
+                for (j in 2..(n / i)) {
+                    is_p[i * j] = false
+                }
+            }
+        }
+    }
+    return ps
 }
 
 fun main(args: Array<String>) {
