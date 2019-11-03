1327f24401f3bf57aa0d528390628812608568aa
emergent <yosikawa@gmail.com>
2019-11-04 01:11:21 +0900

    refactor problem003.cpp

--- C++/problem003.cpp
+++ C++/problem003.cpp
@@ -3,67 +3,20 @@
  * http://projecteuler.net/index.php?section=problems&id=003
  */
 #include <iostream>
-#include <algorithm>
-#include <vector>
-#include <cmath>
-
-template <typename T>
-void print(std::vector<T> v) {
-    std::cout << '[';
-    for (const auto i : v) {
-        std::cout << i << ' ';
-    }
-    std::cout << ']' << std::endl;
-}
-
-template <typename T>
-void print(T x) {
-    std::cout << x << std::endl;
-}
-
-std::vector<long>
-sieve(long n, long maxn, std::vector<long> &v) {
-    long k = v[n];
-    if (k > maxn) {
-        return v;
-    } else {
-        std::vector<long> copied = {};
-        std::copy_if(v.cbegin(), v.cend(), std::back_inserter(copied), [k](long x) { return x==k || x % k != 0; });
-        return sieve(n+1, maxn, copied);
-    }
-}
-
-std::vector<long> primes(long x) {
-    if (x < 2) {
-        return {};
-    } else if (x == 2) {
-        return {2};
-    } else {
-        std::vector<long> v = {};
-        for (int i = 3; i < x+1; i+=2) {
-            v.push_back(i);
-        }
-        v = sieve(0, std::floor(std::sqrt(x)+1), v);
-        std::reverse(v.begin(), v.end());
-        v.push_back(2);
-        std::reverse(v.begin(), v.end());
-        return v;
-    }
-}
 
 long maxprimefactor(long x) {
-    std::vector<long> v = primes(std::floor(std::sqrt(x)));
-    std::reverse(v.begin(), v.end());
-
-    for (const auto p : v) {
-        if (x % p == 0) {
-            return p;
+    long i = 2;
+    while (i * i <= x) {
+        if (x % i == 0) {
+            x /= i;
+        } else {
+            i++;
         }
     }
-    return 1;
+    return x;
 }
 
 int main(int argc, char **argv) {
-    std::cout << maxprimefactor(600851475143) << std::endl;
+    std::cout << maxprimefactor(600851475143L) << std::endl;
     return 0;
 }
