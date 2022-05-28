57e3a5b5b3d7fcfe82d8d451311349fa11738dd0
emergent <yosikawa@gmail.com>
2022-05-29 03:26:59 +0900

    refactor 31 in Rust

--- Rust/src/bin/031.rs
+++ Rust/src/bin/031.rs
@@ -1,33 +1,31 @@
 /// Problem 31 - Project Euler
 /// http://projecteuler.net/index.php?section=problems&id=31
+use derive_new::new;
 
 fn main() {
-    let lim = 200 + 1;
-    let p = (0..lim)
-        .step_by(100)
-        .map(|a| {
-            (0..(lim - a))
-                .step_by(50)
-                .map(|b| {
-                    (0..(lim - (a + b)))
-                        .step_by(20)
-                        .map(|c| {
-                            (0..(lim - (a + b + c)))
-                                .step_by(10)
-                                .map(|d| {
-                                    (0..(lim - (a + b + c + d)))
-                                        .step_by(5)
-                                        .map(|e| {
-                                            (0..(lim - (a + b + c + d + e))).step_by(2).count()
-                                        })
-                                        .fold(0, |s, x| s + x)
-                                })
-                                .fold(0, |s, x| s + x)
-                        })
-                        .fold(0, |s, x| s + x)
-                })
-                .fold(0, |s, x| s + x)
-        })
-        .fold(0, |s, x| s + x);
-    println!("{}", p + 1);
+    let coins = vec![200, 100, 50, 20, 10, 5, 2, 1];
+    let mut p = Pattern::new(0, coins);
+    p.count(200, 0);
+    println!("{}", p.ans);
+}
+
+#[derive(new)]
+struct Pattern {
+    ans: usize,
+    coins: Vec<u64>,
+}
+
+impl Pattern {
+    fn count(&mut self, rest: u64, d: usize) {
+        if rest == 0 || d == self.coins.len() - 1 {
+            self.ans += 1;
+            return;
+        }
+
+        let mut i = 0;
+        while i * self.coins[d] <= rest {
+            self.count(rest - i * self.coins[d], d + 1);
+            i += 1;
+        }
+    }
 }
