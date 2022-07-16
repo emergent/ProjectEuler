3e89aa8cfb40d90135b72712e5aa9b7a94fb0984
emergent <yosikawa@gmail.com>
2022-07-16 15:18:01 +0900

    Denoのimport記法に合わせた

--- TypeScript/problem007.ts
+++ TypeScript/problem007.ts
@@ -2,6 +2,6 @@
  * #7 10001st prime - Project Euler
  * http://projecteuler.net/index.php?section=problems&id=7
  */
-import { sieve } from "./prime";
+import { sieve } from "./prime.ts";
 const primes = sieve(200000);
 console.log(primes[10000]);
--- TypeScript/problem010.ts
+++ TypeScript/problem010.ts
@@ -2,6 +2,6 @@
  * #10 Summation of primes - Project Euler
  * http://projecteuler.net/index.php?section=problems&id=10
  */
-import { sieve } from "./prime";
+import { sieve } from "./prime.ts";
 const ans = sieve(2_000_000).reduce((acc, x) => acc + x);
 console.log(ans);
