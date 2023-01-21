a25d0a69ebecbcaa2ad9ca3e4af93c335cd0caa3
emergent <yosikawa@gmail.com>
2023-01-21 16:50:01 +0900

    update 034.rs

--- Rust/src/bin/034.rs
+++ Rust/src/bin/034.rs
@@ -1,23 +1,21 @@
 /// Problem 34 - Project Euler
 /// http://projecteuler.net/index.php?section=problems&id=34
-fn fac(x: i32) -> i32 {
-    match x {
-        0 => 1,
-        _ => x * fac(x - 1),
-    }
-}
-
-fn facdigits(x: i32) -> i32 {
+fn facdigits(fs: &[i32], x: i32) -> i32 {
     let mut xd = x;
     let mut sum = 0;
     while xd != 0 {
-        sum += fac(xd % 10);
+        sum += fs[(xd % 10) as usize];
         xd /= 10;
     }
     sum
 }
 
 fn main() {
-    let ans: i32 = (3..(fac(9) * 7)).filter(|&i| i == facdigits(i)).sum();
+    let mut fs = [1; 10];
+    for i in 1..=9 {
+        fs[i] = fs[i - 1] * i as i32;
+    }
+
+    let ans: i32 = (3..(fs[9] * 7)).filter(|&i| i == facdigits(&fs, i)).sum();
     println!("{}", ans);
 }
