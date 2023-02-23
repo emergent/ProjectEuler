197420f92f97e3d3b93582b85a7bbff16d1fe6c6
emergent <yosikawa@gmail.com>
2023-02-23 21:50:26 +0900

    add lib BigInt

---
+++ Go/my/bignum.go
@@ -0,0 +1,98 @@
+package my
+
+import (
+	"reflect"
+)
+
+type BigInt struct {
+	inner []int
+}
+
+func NewBigInt(x int) *BigInt {
+	inner := []int{}
+	for x > 0 {
+		inner = append(inner, x%10)
+		x /= 10
+	}
+	return &BigInt{inner}
+}
+
+func (bi *BigInt) Equal(rhs *BigInt) bool {
+	if bi == nil || rhs == nil {
+		return false
+	}
+	return reflect.DeepEqual(bi.inner, rhs.inner)
+}
+
+func (bi *BigInt) Len() int {
+	if bi == nil {
+		return 0
+	}
+	return len(bi.inner)
+}
+
+func (bi *BigInt) Digits() []int {
+	if bi == nil {
+		return []int{}
+	}
+	return bi.inner
+}
+
+func (bi *BigInt) Add(rhs *BigInt) *BigInt {
+	if bi == nil {
+		return nil
+	}
+	if rhs == nil {
+		return bi
+	}
+
+	l_len := len(bi.inner)
+	r_len := len(rhs.inner)
+	carried := 0
+	for i := 0; i < l_len; i++ {
+		if i < r_len {
+			d := bi.inner[i] + rhs.inner[i] + carried
+			bi.inner[i] = d % 10
+			carried = d / 10
+		} else {
+			d := bi.inner[i] + carried
+			bi.inner[i] = d % 10
+			carried = d / 10
+		}
+	}
+
+	for i := l_len; i < r_len; i++ {
+		d := rhs.inner[i] + carried
+		bi.inner = append(bi.inner, d%10)
+		carried = d / 10
+	}
+
+	for carried > 0 {
+		bi.inner = append(bi.inner, carried%10)
+		carried /= 10
+	}
+
+	return bi
+}
+
+func (bi *BigInt) MulInt(rhs int) *BigInt {
+	if bi == nil {
+		return nil
+	}
+
+	l_len := len(bi.inner)
+	carried := 0
+	for i := 0; i < l_len; i++ {
+		d := bi.inner[i]*rhs + carried
+		bi.inner[i] = d % 10
+		carried = d / 10
+	}
+
+	for carried > 0 {
+		bi.inner = append(bi.inner, carried%10)
+		carried /= 10
+	}
+
+	return bi
+
+}
---
+++ Go/my/bignum_test.go
@@ -0,0 +1,44 @@
+package my
+
+import "testing"
+
+func helperAdd(l, r, expect int, t *testing.T) {
+	bl := NewBigInt(l)
+	br := NewBigInt(r)
+	be := NewBigInt(expect)
+	bl = bl.Add(br)
+	if !bl.Equal(be) {
+		t.Errorf("Addition of %v,%v = %v, want = %v", l, r, l+r, expect)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	helperAdd(1, 2, 3, t)
+	helperAdd(9, 2, 11, t)
+	helperAdd(99, 2, 101, t)
+	helperAdd(999, 2, 1001, t)
+
+	helperAdd(222, 222, 444, t)
+	helperAdd(777, 333, 1110, t)
+
+	helperAdd(2, 9, 11, t)
+	helperAdd(2, 99, 101, t)
+	helperAdd(2, 999, 1001, t)
+}
+
+func helperMulInt(l, r, expect int, t *testing.T) {
+	bl := NewBigInt(l)
+	be := NewBigInt(expect)
+	bl = bl.MulInt(r)
+	if !bl.Equal(be) {
+		t.Errorf("Multiplication of %v,%v = %v, want = %v", l, r, l+r, expect)
+	}
+}
+
+func TestMulInt(t *testing.T) {
+	helperMulInt(1, 1, 1, t)
+	helperMulInt(1, 2, 2, t)
+	helperMulInt(2, 2, 4, t)
+	helperMulInt(16, 16, 256, t)
+	helperMulInt(9, 12345679, 111111111, t)
+}
