323b14de4be177a94ac00bb8fac0dbbf0c0f297e
emergent <yosikawa@gmail.com>
2023-06-10 13:42:48 +0900

    refactor

--- Python/problem012.py
+++ Python/problem012.py
@@ -1,27 +1,28 @@
 #! /usr/bin/env python3
-'''
+"""
 Problem 12 - Project Euler
 http://projecteuler.net/index.php?section=problems&id=012
-'''
-import math
+"""
 
-def getTriangleNumber(i):
-    return int(((i+1)*(i+2))/2)
 
-def getDivisors(x):
+def get_divisors(x):
     dv = []
-    for i in range(1, int(math.sqrt(x))+1):
+    i = 1
+    while i * i <= x:
         if x % i == 0:
             dv.append(i)
             dv.append(x // i)
+        i += 1
     return list(set(dv))
 
-if __name__ == '__main__':
-    i = 0
+
+if __name__ == "__main__":
+    num = 1
+    i = 1
     while True:
-        tn = getTriangleNumber(i)
-        dv = getDivisors(tn)
-        if (len(dv) > 500):
-            print(tn, len(dv))
+        dv = get_divisors(num)
+        if len(dv) > 500:
+            print(num, len(dv))
             break
         i += 1
+        num += i
