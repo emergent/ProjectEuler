b244e52833ed8c7b674772c122e539a74c51f4c2
emergent <yosikawa@gmail.com>
2025-01-04 23:52:30 +0900

    fix warning

--- TypeScript/problem010.ts
+++ TypeScript/problem010.ts
@@ -4,6 +4,6 @@
  */
 import { sieve } from "./prime.ts";
 {
-    const ans = sieve(2_000_000).reduce((acc, x) => acc + x);
+    const ans = sieve(2_000_000).reduce((acc, x) => acc + x, 0);
     console.log(ans);
 }
